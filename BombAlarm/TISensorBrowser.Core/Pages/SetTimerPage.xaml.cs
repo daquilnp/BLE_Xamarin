8fa390b09104ca4a000c38244eb1e9db806f29ec
Nicholas D'Aquila <daquilnp@mcmaster.ca>
2016-07-20 15:28:02 -0400

    Timer Pick Content Page opened after connected to Bean and initialized characteristics

--- BombAlarm/TISensorBrowser.Core/Pages/SetTimerPage.xaml.cs
+++ BombAlarm/TISensorBrowser.Core/Pages/SetTimerPage.xaml.cs
@@ -1,16 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace TISensorBrowser
 {
 	public partial class SetTimerPage : ContentPage
 	{
+		DateTime triggerTime;
+
 		public SetTimerPage ()
 		{
-			InitializeComponent ();
-		}
+			InitializeComponent(); 
+			Device .StartTimer( TimeSpan.FromSeconds(1), OnTimerTick); 
+		} 
+		bool OnTimerTick() {
+
+			if (t_switch.IsToggled && DateTime.Now >= triggerTime) 
+			{ 
+				t_switch.IsToggled = false ; DisplayAlert( "Timer Alert" , "The '" + entry.Text + "' timer has elapsed" , "OK" );
+			} 
+			return true ; 
+		} 
+		void OnTimePickerPropertyChanged( object obj, PropertyChangedEventArgs args)
+		{ if (args.PropertyName == "Time" ) {
+				SetTriggerTime(); 
+			} 
+		} 
+		void OnSwitchToggled( object obj, ToggledEventArgs args) {
+			SetTriggerTime();
+		} 
+		void SetTriggerTime() { 
+			if (t_switch.IsToggled) { 
+				triggerTime = DateTime.Today + timePicker.Time; 
+				if (triggerTime < DateTime.Now) { 
+					triggerTime += TimeSpan.FromDays(1); 
+				} 
+			} 
+		} 
 	}
 }
-
