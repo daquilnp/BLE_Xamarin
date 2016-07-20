4372ea5dfd1f7cd30202ad0d2bdd06a04673716f
Nicholas D'Aquila <daquilnp@mcmaster.ca>
2016-07-20 15:22:22 -0400

    Added the TimePicker that allows user to pick time and then get an alert message when the picked time equals current time.

---
+++ BombAlarm/TISensorBrowser.Core/Pages/SetTimerPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TISensorBrowser
+{
+	public partial class SetTimerPage : ContentPage
+	{
+		public SetTimerPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
