1f2d9b3dde8311b292e993387a63a8ced9d83e4a
Nicholas D'Aquila <daquilnp@mcmaster.ca>
2016-07-25 21:05:03 -0400

    Added bash script to install Node Red and Bean Nodes

---
+++ Bean_Node-Red_Install_Script/node_red_bean_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install npm
+sudo npm install -g --unsafe-perm node-red
+npm install noble
+mkdir -p ~/.node-red/node_modules
+npm install --prefix ~/.node-red node-red-contrib-bean
